0b1fb8a78aec004a51984263fd5840656c73b24e
advaith-1001 <125677779+advaith-1001@users.noreply.github.com>
2025-08-01 21:23:54 +0530

    origin update

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://doc-diff-ten.vercel.app/"],
+    allow_origins=["https://doc-diff-ten.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
